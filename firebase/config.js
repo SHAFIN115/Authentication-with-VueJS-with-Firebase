aace5ca571d8669d3f721fa478370e38b7a68097
shafin <37385149+SHAFIN115@users.noreply.github.com>
2023-04-12 03:27:48 +0600

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -2,10 +2,10 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCCTjVIQpZq4AkTUleVVovk35qxtUk_kgw",
-  authDomain: "fir-auth-f1414.firebaseapp.com",
-  projectId: "fir-auth-f1414",
-  storageBucket: "fir-auth-f1414.appspot.com",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
   messagingSenderId: "95676861680",
   appId: "1:95676861680:web:843f0db7175e95b4084635",
   measurementId: "G-K265W163T2"
